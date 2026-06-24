666ce729bcdbedfd918f75c55e0d88dabc12e9d5
Adil Inamdar <adilinamdar617@gmail.com>
2026-06-24 20:31:09 +0530

    Update index.js

--- 1.QuickBite/backend/index.js
+++ 1.QuickBite/backend/index.js
@@ -17,7 +17,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://mern-showcase.vercel.app",
     credentials: true,
     methods: ["POST", "GET"],
   },
@@ -28,7 +29,8 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://mern-showcase.vercel.app",
     credentials: true,
   }),
 );
